0a2bc2d7862244ccfb4fd7aeb12f3a05dd71d5f6
Leo Ding <leoding86@msn.com>
2017-01-09 22:55:09 +0800

    update & add
    update 修改接口download实现以及配置实现
    add 新增build方法
    add 新增创建入口方法

--- Service/Tumblr/Service.php
+++ Service/Tumblr/Service.php
@@ -1,34 +1,33 @@
 <?php
 namespace Service\Tumblr;
 
-use \Common\Helper;
-use \Common\Downloader;
-use \Common\DownloadPool;
+use Common\DumperException;
+use Common\Helper;
+use Common\Downloader;
+use Common\DownloadPool;
+use Webmozart\PathUtil\Path;
+
+define('SERVICE', 'Tumblr'); // 定义服务名称
 
 class Service extends \Common\Service
 {
-    /**
-     * Cli command line example :
-     * php service.php -S Tumblr -k [API_KEY] -a dump -s [SAVE_PATH] -p 127.0.0.1:1080 -b [BLOG1,BLOG2] -t [POST_TYPE]
-     */
-    private $tumblr = null;
-    protected $args   = [];
+    private $tumblr  = null;
+    private $saveDir = null;
+    private $apiKey  = null;
+    private $action  = null;
+    private $blogs   = [];
+    private $postType = null; 
     protected $argsValidation = [
         ['k', '/^[a-z\d]{32,50}$/i',       'api_key is invalid'],
-        ['a', '/^dump|save$/',             'unkown action'],
-        ['b', '/^([a-z\d\-_]+\.tumblr\.com,?)+$/', 'invalid blog identifier'],
-        ['p', '/^.+$/',                    'invalid proxy'],
-        ['s', '/^.+$/',                    'invalid save path'],
+        ['a', '/^dump|save|build$/',             'unkown action'],
+        ['b', '/^([a-z\d_-]+\.tumblr\.com,?)+$/', 'invalid blog identifier'],
         ['t', '/^[a-z]+$/',                'invalid post type'],
-        ['r', '/^\d+$/',                   'invalid retry times'],
     ];
-    protected $saveDir    = null;
-    protected $retryTimes = 3;
-    protected $startTime = 0;
-    protected $endTime = 0;
 
     protected function parseArgs($args)
     {
+        $this->args = []; // 重置参数集合
+
         foreach ($args as $name => $value) {
             foreach ($this->argsValidation as $_arg) {
                 if ($name == $_arg[0] && !preg_match($_arg[1], $value)) {
@@ -39,29 +38,45 @@ class Service extends \Common\Service
             $this->args[$name] = $value;
         }
 
+        /* apiKey */
+        if (!isset($this->args['k'])) {
+            Helper::printlnExit('Api key is not valid');
+        } else {
+            $this->apiKey = $this->args['k'];
+        }
+
+        /* 行为 */
+        if (!isset($this->args['a'])) {
+            Helper::printlnExit('Invalid action');
+        } else {
+            $this->action = $this->args['a'];
+        }
+
+        /* 处理博客设置 */
         if (!isset($this->args['b'])) {
             Helper::printlnExit('Blog has not been setted');
+        } else {
+            $this->blogs = explode(',', $this->args['b']);
         }
 
-        if (!isset($this->args['s'])) {
-            Helper::printlnExit('Save path has not been setted');
-        }
-
-        if (isset($this->args['r']) && $this->args['r'] > 0) {
-            $this->retryTimes = $this->args['r'];
+        /* 文章类型 */
+        if (isset($this->args['t'])) {
+            if (!in_array($this->args['t'], ['photo', 'video'])) {
+                Helper::printlnExit('Invalid post type');
+            }
         }
     }
 
-    private function getType()
-    {
-        return isset($this->args['t']) ? $this->args['t'] : null;
-    }
-
+    /**
+     * 创建博客文件夹
+     * 
+     * @param  string $blog 博客名字
+     * @return void
+     */
     private function createBlogSaveFolder($blog)
     {
-        $this->args['s'] .= substr($this->args['s'], -1) == '/' ? '' : '/';
-        if (is_dir($this->args['s'])) {
-            $this->saveDir = $this->args['s'] . $blog . '/';
+        if (is_dir($this->saveRootDir)) {
+            $this->saveDir = $this->saveRootDir . '/' . $blog;
             if (!is_dir($this->saveDir)) {
                 /* 创建目录 */
                 mkdir($this->saveDir);
@@ -71,15 +86,22 @@ class Service extends \Common\Service
         }
     }
 
+    /**
+     * 创建保存文件的文章文件夹
+     * 
+     * @param  string $post_id 文章id
+     * @throws \Common\DumperException dumper异常
+     * @return string                  文件夹路径
+     */
     private function createPostSaveFolder($post_id)
     {
         if (empty($post_id)) {
-            throw new \Exception("post_id missing for creating folder for saving post");
+            throw new DumperException("post_id missing for creating folder for saving post");
         }
 
-        $save_dir = $this->saveDir . $post_id . '/';
+        $save_dir = $this->saveDir . '/' . $post_id . '/';
         if (!is_dir($save_dir) && mkdir($save_dir) === false) {
-            throw new \Exception("Cannot create folder for saving post [" . $save_dir . "]");
+            throw new DumperException("Cannot create folder for saving post [" . $save_dir . "]");
         }
 
         return $save_dir;
@@ -91,7 +113,7 @@ class Service extends \Common\Service
         while ($retry_times-- > 0) {
             $posts = null;
             try {
-                $posts = $this->tumblr->setParams($params)->posts($blog, $this->getType());
+                $posts = $this->tumblr->setParams($params)->posts($blog, $this->postType);
                 if ($posts == null) {
                     Helper::println('Get posts error, skip');
                     yield null;
@@ -150,15 +172,57 @@ class Service extends \Common\Service
         }
     }
 
+    /**
+     * 生成入口页面
+     * 
+     * @return void
+     */
+    private function createEntryPage()
+    {
+        $directory = new \Common\Directory($this->saveDir);
+        $directories = $directory->getDirectories();
+        $posts = [];
+
+        $photo_fliter = new \Common\Directory\Fliter(['jpg', 'jpeg', 'gif', 'png', 'bmp']);
+        $video_fliter = new \Common\Directory\Fliter(['mp4', 'flv', 'swf']);
+        $text_fliter = new \Common\Directory\Fliter(['txt']);
+        foreach ($directories as $dir) {
+            $post_info = [];
+// Helper::println($dir);
+            $directory->setDir($dir);
+            if (!empty($directory->fliteFiles($photo_fliter)->getFiles())) {
+                $post_info = ['type' => 'photo', 'count' => count($directory->getFiles())];
+            } else if (!empty($directory->fliteFiles($video_fliter)->getFiles())) {
+                $post_info = ['type' => 'video', 'count' => count($directory->getFiles())];
+            } else if (!empty($directory->fliteFiles($text_fliter)->getFiles())) {
+                $post_info = ['type' => 'text', 'count' => count($directory->getFiles())];
+            }
+
+            if (!empty($post_info)) {
+                $post_info['url'] = Path::makeRelative($dir, $this->saveDir) . '/index.html';
+                $posts[] = $post_info;
+            }
+        }
+
+        $template = new \Common\Template();
+        $template->setTemplate('entry')
+                 ->assign('posts', $posts)
+                 ->render();
+        $html_handle = fopen($this->saveDir . '/index.html', 'w');
+        fwrite($html_handle, $template->getContent());
+        fclose($html_handle);
+    }
+
     private function dump()
     {
-        foreach (explode(',', $this->args['b']) as $blog) {
+        foreach ($this->blogs as $blog) {
             $retry_times = $this->retryTimes;
+            $max_count = $this->maxCount;
 
             do {
                 try {
                     Helper::println('Try to get blog %s post count', $blog);
-                    $info = $this->tumblr->posts($blog, $this->getType());
+                    $info = $this->tumblr->posts($blog, $this->postType);
                     break;
                 } catch (\Exception $e) {
                     Helper::println($e->getMessage() . ', retry.');
@@ -178,14 +242,17 @@ class Service extends \Common\Service
 
             /* 创建线程池 */
             $loaders = [require('./vendor/autoload.php'), \Autoloader::getLoader()];
-            $pool = new DownloadPool(50, DownloadWorker::class, [$loaders]);
+            $pool = new DownloadPool(3, DownloadWorker::class, [$loaders]);
 
             /* 循环获得文章内容 */
             for ($offset = 0; $offset < $info->total_posts; $offset += 20) {
-
                 Helper::println('%s %s - %s', $blog, $offset, $offset + 20);
 
                 foreach ($this->getPosts($blog, ['offset' => $offset]) as $post) {
+                    if ($this->maxCount > 0 && $max_count-- < 0) {
+                        break 2;
+                    }
+
                     if (is_null($post)) {
                         continue;
                     }
@@ -206,19 +273,31 @@ class Service extends \Common\Service
 
             $pool->process(); // 同步所有任务
             $pool->shutdown();
+            Helper::println('Create ' . $blog . ' entry page file');
+            $this->createEntryPage();
         }
-
         Helper::printlnExit('Completed!');
+    }
+
+    public function build()
+    {
+        foreach ($this->blogs as $blog) {
+            $this->createBlogSaveFolder($blog);
+            Helper::println('Create ' . $blog . ' entry page file');
+            $this->createEntryPage();
+            Helper::printlnExit('Completed!');
+        }
     }
 
     public function __construct($args)
     {
+        parent::__construct($args);
         $this->parseArgs($args);
-        $this->tumblr = new Api($this->args['k']);
+        $this->tumblr = new Api($this->apiKey);
         $this->tumblr->addOptions(['timeout' => 60]);
 
-        if (isset($this->args['p'])) {
-            $this->tumblr->addOptions(['proxy' => $this->args['p']]);
+        if (!is_null($this->proxy)) {
+            $this->tumblr->addOptions(['proxy' => $this->proxy]);
         }
     }
 
@@ -233,15 +312,12 @@ class Service extends \Common\Service
             Helper::println('Job %s is complete %f%%', $job_id, $completed_size / $total_size * 100);
         });
         $downloader->registerHook(Downloader::COMPLETE_EVENT, function($job_id, $is_skip) {
-            Helper::println('Job %s is completed [%s]', $job_id, ($is_skip ? 'skip' : ''));
+            Helper::println('Job %s is completed %s', $job_id, ($is_skip ? '[skip]' : ''));
         });
     }
 
     public function start()
     {
-        if (!isset($this->args['a'])) {
-            Helper::println("Has no action", true);
-        }
         $this->startTime = time();
         call_user_func([$this, $this->args['a']]);
         $this->endTime = time();
